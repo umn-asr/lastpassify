bbf888b5e37b80830be37d66caf4e262b08bf036
Ian Whitney <whit0694@umn.edu>
2021-01-25 12:30:33 -0600

    Remove binstubs, update scripts
    To help diagnose the warnings we get about lastpassify including
    duplicate executables, we are removing the binstub directory. Code that
    was using the binstub path has been updated to use `bundle exec`
    
    Co-authored-by: Remy Abdullahi <abdu0299@umn.edu>

--- spec/lib/lastpassify_spec.rb
+++ spec/lib/lastpassify_spec.rb
@@ -10,29 +10,29 @@ RSpec.describe LastPassify::LastPassify, type: :aruba do
     end
 
     it "copies the output file to config/database.yml" do
-      run "bundle exec lastpassify #{input}"
+      run_command "bundle exec lastpassify #{input}"
       expect(output).to be_an_existing_file
       expect(output).to have_file_content(/Ex@mple_p@ssw0rd\#\$/)
     end
 
     it "doesn't copy production block" do
-      run "bundle exec lastpassify #{input}"
+      run_command "bundle exec lastpassify #{input}"
       expect(output).not_to have_file_content(/production/)
     end
 
     it "doesn't copy staging block" do
-      run "bundle exec lastpassify #{input}"
+      run_command "bundle exec lastpassify #{input}"
       expect(output).not_to have_file_content(/staging|qat/)
     end
 
     it "retrieves values for keys that contain spaces" do
-      run "bundle exec lastpassify #{input}"
+      run_command "bundle exec lastpassify #{input}"
       expect(output).to have_file_content(/secret_key: test/)
     end
 
     context "includes production flag" do
       before :each do
-        run "bundle exec lastpassify #{input} -p"
+        run_command "bundle exec lastpassify #{input} -p"
       end
 
       it "copies production block" do
@@ -42,7 +42,7 @@ RSpec.describe LastPassify::LastPassify, type: :aruba do
 
     context "includes staging flag" do
       before :each do
-        run "bundle exec lastpassify #{input} -s"
+        run_command "bundle exec lastpassify #{input} -s"
       end
 
       it "copies staging block" do
@@ -52,7 +52,7 @@ RSpec.describe LastPassify::LastPassify, type: :aruba do
 
     context "output file is set" do
       before :each do
-        run "bundle exec lastpassify #{input} some_other_file.yml"
+        run_command "bundle exec lastpassify #{input} some_other_file.yml"
       end
 
       it "creates output filename" do
